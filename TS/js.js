cbeb7f7c5e76ed7784ac3681405603e746bc24ad
Vitor <132232270+BomNome@users.noreply.github.com>
2023-05-30 21:16:31 -0300

    Add files via upload

---
+++ TS/js.js
@@ -0,0 +1,8 @@
+let a = prompt();
+let b = prompt();
+
+function soma(a, b){
+    return a + b;
+}
+
+document.write(soma(a + b));
---
+++ TS/ts.ts
@@ -0,0 +1,5 @@
+function soma(a: number, b: number){
+    return a + b;
+}
+
+document.write(soma(a, b));
